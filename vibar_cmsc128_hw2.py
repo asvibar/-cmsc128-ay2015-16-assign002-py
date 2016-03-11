9ff5f15d5fd8ad40144dff42d25b8158aa50deed
asvibar <asvibar@up.edu.ph>
2016-03-12 04:02:58 +0800

    Code fix after testing

--- vibar_cmsc128_hw2.py
+++ vibar_cmsc128_hw2.py
@@ -1,5 +1,12 @@
+#=================================#
+#   Aron John S. Vibar            #
+#   2013-03369 CMSC 128 AB-1L     #
+#   2nd Programming Assignment    #
+#=================================#
+
+#Returns the "Hamming Distance" of two strings of the same length
 def getHammingDistance(a,b):
-    hd = 0
+    hd = 0 #hamming distance
     if len(a) <= 0 | len(b) <= 0:
         print("Length of any of the strings must not be zero")
     elif len(a) != len(b):
@@ -11,49 +18,54 @@ def getHammingDistance(a,b):
 
     return hd
     
+#Returns the number of occurences of a given pattern in a given string
 def countSubstrPattern(original,pattern):
-    occ = 0
-    if original.find(pattern,beg=0,end=len(original)) != -1:
-        for i in range(len(original)-len(pattern)):
-            if original.find(pattern,beg=i,end=len(pattern)) != -1:
+    occ = 0 #number of occurence
+    if original.find(pattern,0,len(original)) != -1:
+        for i in range(len(original)-len(pattern)+1):
+            if original.find(pattern,i,len(pattern)+i) != -1:    
                 occ+=1
     return occ
 
+#Returns true if all chars of the string uses the given alphabet,false otherwise
 def isValidString(string,alphabet):
-    for a in alphabet:
-        if a not in string:
-            return false
+    for a in string:
+        if a not in alphabet:
+            return False
     
-    return true
+    return True
 
+#Returns the # of skews from the 1st element of the string to the nth element
 def getSkew(string, n):
-    gcount = 0
-    ccount = 0
-
-    if n <= len(string):
-        for i in range(n-1):
+    gcount = 0 #number of G's encountered
+    ccount = 0 #number of C's encountered
+    skew = 0
+    if n < len(string):
+        for i in range(n):
             if string[i] == "G":
                 gcount+=1                                 
             elif string[i] == "C":
                 ccount+=1
-                
-    return gcount-ccount                 
-    
+    skew = gcount-ccount            
+    return skew
+            
+#Returns the max. skews from the 1st element of the string to each nth element
 def getMaxSkew(string, n):
-    skewlist = []
-    skew = 0
-    for i in range(n-1):
-        skew = getSkew(string,n)
-        skewlist.append(skew)
-    return max(skewlist)
+    skewlist = [] #list of the number of skews from 1 to each n
+    if len(string) <= 0:
+        return 0
+    else:
+        for i in range(n):
+            skewlist.append(getSkew(string,i+1))
+            return max(skewlist)
 
+#Returns the min. skews from the 1st element of the string to each nth element
 def getMinSkew(string, n):
     skewlist = []
-    skew = 0    
-    for i in range(n-1):
-        skew = getSkew(string,n)
-        skewlist.append(skew)
-    return min(skewlist)
+    if len(string) <= 0:
+        return 0
+    else:
+        for i in range(n):
+            skewlist.append(getSkew(string,i+1))
+        return min(skewlist)    
     
-    
-                 
